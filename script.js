554ce4bb2e7c7e0269be8c1bf2ab54bb3c89c384
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-04-07 13:56:29 -0700

    Add operate function
    this function takes 2 numbers and a operator symbol and performs the
    mathmatical function respective to the operator symbol on the 2 numbers.

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1/num2;
 }
+
+// Operation variables
+let num1;
+let num2;
+let operator;
+
+// operate function
+function operate(num1, operator, num2) {
+    switch (operator) { 
+        case "+":
+            return add(num1,num2);
+            break;
+        case "-":
+            return subtract(num1,num2);
+            break;
+        case "*":
+            return multiply(num1,num2);
+            break;
+        case "/":
+            return divide(num1,num2);
+            break;
+    }
+}
