890a17f77687583c13e52d8ae7bca5aae6c9c5d0
Jennifer H <74622778+jh80@users.noreply.github.com>
2024-04-15 08:51:32 -0700

    Merge activateButton() to work on key&mouseclicks
    I originally used two different functions for the key presses and the
    mouseclicks because it didn't seem simple to sub the differences out for
    a variable. The functions, however, reuse a lot of the same code, so I
    figured out how to merge the functions.

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const tooBigMsg = "# TOO BIG";
 const displayFit = 13;
 // Button click event
 const btnContainer = document.querySelector("#btnContainer");
-btnContainer.addEventListener('click', activateButton);
-document.body.addEventListener('keydown', activateOnKeyDown);
+btnContainer.addEventListener('click',  (event) => activateButton(event.target));
+document.body.addEventListener('keydown', (event) => activateButton(event.key));
 
 // Operator functions
 function add(num1, num2) {
@@ -71,71 +71,6 @@ function populateDisplay(digitStr, displayValue = "") {
     return display.textContent;
 }                 
 
-// ACTIVATEBUTTON FUNCTION
-function activateButton (event) {
-    // Force user to clear if an alphabetical string has been displayed
-    const target = event.target;
-    if (isNaN(display.textContent)) {
-        if (target.id !== "clear") {
-            return;
-        } 
-    }
-    if (target.className === "btn number") { // For numbers
-        displayValue = populateDisplay(target.textContent, displayValue);
-        backspace = true;
-    } else if (target.id === "decimalPoint") { // For decimal points
-        if (displayValue.includes(".")) {
-            return;
-        }
-        displayValue = populateDisplay(".", displayValue);
-    } else if (target.className === "btn operator") { // For operators
-        if (num1 === undefined) {
-            if (display.textContent) {
-            num1 = Number(display.textContent);
-            operator = target.textContent;
-            displayValue = "";
-            }
-        } else {
-            equalOut();
-            displayValue = "";
-            backspace = false;
-            operator = target.textContent;
-        }
-    } else if (target.id === "equals") { // For equals
-        if (num1 === undefined) {
-            displayValue = "";
-            return;
-        }
-        equalOut();
-        num1 = undefined;
-        displayValue = "";  
-        backspace = false;
-        operator = "";    
-    } else if (target.id === "clear") { // For clear
-        refresh();
-        populateDisplay("0");
-    } else if (target.id === "backspace") { // For backspace
-        if(backspace) {
-            displayValue = populateDisplay(display.textContent.slice(0,-1));
-        }
-    } else if (target.id === "negative") { // For negative sign
-        if (display.textContent.includes("-")) {
-            display.textContent = display.textContent.slice(1);
-            displayValue = display.textContent;
-        } else {
-            if (display.textContent === "0") {
-                display.textContent = "-0";
-                displayValue = "-"
-            } else {
-                display.textContent = "-" + display.textContent;
-                displayValue = display.textContent;
-            }
-        }
-    }
-}
-
-
-
 // Rounds number to a specified amount of digits (on both sides of decimal point
 function roundToDigits(num, digits = 0, notice = "tooManyDigits") {
     // For my needs digits should be 13
@@ -150,70 +85,6 @@ function roundToDigits(num, digits = 0, notice = "tooManyDigits") {
         return notice;
     } else {
         return num;
-    }
-}
-// function for keydown
-function activateOnKeyDown (event) {
-    const DIGITS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-    const OPERATORS = ["+", "-", "*", "/"]
-    const target = event.key;
-    if (isNaN(display.textContent)) {
-        if (target !== "c") {
-            return;
-        } 
-    }
-    //displayValue = display.textContent;
-    if (DIGITS.includes(target)) {
-        displayValue = populateDisplay(target, displayValue);
-        backspace = true;
-    } else if (target === ".") {
-        if (displayValue.includes(".")) {
-            return;
-        }
-        displayValue = populateDisplay(".", displayValue);
-    } else if (OPERATORS.includes(target)) {
-        if (num1 === undefined) {
-            if (display.textContent) {
-                num1 = Number(display.textContent);
-                operator = target;
-                displayValue = ""
-            }
-        } else {    
-            num2 = Number(display.textContent);
-            num1 = operate(num1, operator, num2);
-            operator = target;
-            if (num1 === "Attempted divide by 0") {
-                populateDisplay("Stop that!");
-                refresh();
-                return;
-            }
-            populateDisplay(String(roundToDigits(num1, 13, tooBigMsg)));
-            displayValue = "";
-            backspace = false;
-        }
-    } else if (target === "Enter" || target === "=") {
-        if (num1 === undefined) {
-            return;
-        }
-        num2 = Number(display.textContent);
-        num1 = operate(num1, operator, num2);
-        operator = "";
-        if (num1 === "Attempted divide by 0") {
-            populateDisplay("Stop that!");
-            refresh();
-            return;
-        }
-        populateDisplay(String(roundToDigits(num1, 13, tooBigMsg)));
-        num1 = undefined;
-        displayValue = "";  
-        backspace = false;    
-    } else if (target === "c") {
-        refresh();
-        populateDisplay("0");
-    } else if (target === "Backspace") {
-        if(backspace) {
-            displayValue = populateDisplay(display.textContent.slice(0,-1));
-        }
     }
 }
 
@@ -234,4 +105,70 @@ function equalOut() {
         return;
     }
     populateDisplay(String(roundToDigits(num1, 13, tooBigMsg)));
+}
+
+// Activate button function for both
+// ACTIVATEBUTTON FUNCTION
+function activateButton (target) {
+    const DIGITS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    const OPERATORS = ["+", "-", "*", "/"]
+    // Force user to clear if an alphabetical string has been displayed
+    if (isNaN(display.textContent)) {
+        if (target.className !== "btn clear" || target !== "c") {// CHANGE TO CLASSNAME
+            return;
+        } 
+    }
+    // Check for buttonpressed
+    if (target.className === "btn number" || DIGITS.includes(target)) { // For numbers
+        displayValue = populateDisplay(DIGITS.includes(target) ? target : target.textContent, displayValue);
+        backspace = true;
+    } else if (target.className === "btn decimalPoint" || target === ".") { // For decimal points
+        if (displayValue.includes(".")) {
+            return;
+        }
+        displayValue = populateDisplay(".", displayValue);
+    } else if (target.className === "btn operator" || OPERATORS.includes(target)) { // For operators
+        if (num1 === undefined) {
+            if (display.textContent) {
+            num1 = Number(display.textContent);
+            operator = OPERATORS.includes(target) ? target : target.textContent;
+            displayValue = "";
+            }
+        } else {
+            equalOut();
+            displayValue = "";
+            backspace = false;
+            operator = OPERATORS.includes(target) ? target : target.textContent;
+        }
+    } else if (target.className === "btn equals" || target === "Enter" || target === "=") { // For equals
+        if (num1 === undefined) {
+            displayValue = "";
+            return;
+        }
+        equalOut();
+        num1 = undefined;
+        displayValue = "";  
+        backspace = false;
+        operator = "";    
+    } else if (target.className === "btn clear" || target === "c") { // For clear
+        refresh();
+        populateDisplay("0");
+    } else if (target.className === "btn backspace" || target === "Backspace") { // For backspace
+        if(backspace) {
+            displayValue = populateDisplay(display.textContent.slice(0,-1));
+        }
+    } else if (target.className === "btn negative") { // For negative sign
+        if (display.textContent.includes("-")) {
+            display.textContent = display.textContent.slice(1);
+            displayValue = display.textContent;
+        } else {
+            if (display.textContent === "0") {
+                display.textContent = "-0";
+                displayValue = "-"
+            } else {
+                display.textContent = "-" + display.textContent;
+                displayValue = display.textContent;
+            }
+        }
+    }
 }
